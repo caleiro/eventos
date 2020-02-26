b9001f27f5507fa26904855c3973989dedbd66d9
Jorge Caleiro <blog.digital01@gmail.com>
2020-02-26 02:19:46 +0000

    Lista_eventos final

--- lista_eventos.php
+++ lista_eventos.php
@@ -1,7 +1,23 @@
 <?php
 include "conexao.php";
 
-$consulta=$conn->query("select * from jmc_evento");
+$registos=$conn->prepare("select * from jmc_evento");
+$registos->execute();
 
+$lista_eventos = $registos->get_result();
 
+$eventos_json = [];
+
+while($evento = $lista_eventos->fetch_assoc())
+{
+  $eventos_jason [] = [
+    'id'=>$evento['id'],
+    'titulo'=>$evento['titulo'],
+    'cor'=>$evento['cor'],
+    'inicio'=>$evento['inicio'],
+    'fim'=>$evento['fim']
+  ];
+}
+
+echo json_encode($eventos_json);
 ?>
